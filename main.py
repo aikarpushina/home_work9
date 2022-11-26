f010c79f1f70ea05feddd97b7de9ff9d62a3d4c2
lyonchikk1 <alena.karpushina2015@yandex.ru>
2022-11-26 19:19:17 +0300

    fix start and append start candy bot

--- main.py
+++ main.py
@@ -10,9 +10,45 @@ import words
 bot = telebot.TeleBot(security.get_token())
 
 
+class LaunchedBot(object):
+    launched_bot = ""
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(LaunchedBot, cls).__new__(cls)
+        return cls.instance
+
+    def get(self) -> str:
+        return self.launched_bot
+
+    def set(self, launched_bot: str) -> None:
+        self.launched_bot = launched_bot
+
+
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    words.word_bot(bot, message)
+    launched_bot = LaunchedBot()
+    print(f"launched_bot = {launched_bot.get()}")
+    print(f"input text = {message.text}")
+    print("----------------------------------------")
+    if message.text == "/word":
+        launched_bot.set(launched_bot="word")
+        bot.send_message(message.from_user.id, f"Бот запущен для помощи введи /help")
+    if message.text == "/candy":
+        launched_bot.set(launched_bot="candy")
+        bot.send_message(message.from_user.id, f"Бот запущен для помощи введи /help")
+    if message.text == "/restart":
+        launched_bot.set(launched_bot="")
+    if launched_bot.get() == "word":
+        words.word_bot(bot, message)
+    if launched_bot.get() == "candy":
+        bot.send_message(message.from_user.id, f"Упс!")
+    if launched_bot.get() == "":
+        bot.send_message(message.from_user.id,
+                         f"Какого бота запустить?\n"
+                         f"Удаление слов введи /word\n"
+                         f"Игра конфеты введи /candy\n"
+                         f"Перезапуск /restart\n")
 
 
 if __name__ == "__main__":
