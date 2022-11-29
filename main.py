e9c7a2c0b73045a7076be2fae6b44483e45db586
lyonchikk1 <alena.karpushina2015@yandex.ru>
2022-11-29 21:03:11 +0300

    fix logger

--- main.py
+++ main.py
@@ -10,15 +10,18 @@ import telebot
 import words
 import candies
 import logging
+from logging.handlers import TimedRotatingFileHandler
 
 bot = telebot.TeleBot(security.get_token())
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    filename="mylog.log",
-    format="%(asctime)s - %(module)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s",
-    datefmt='%Y-%m-%d %H:%M:%S',
-    )
+file_name = r"logs/mylog.log"
+
+logger = logging.getLogger("root loger")
+logger.setLevel(logging.INFO)
+
+handler = TimedRotatingFileHandler(filename=file_name, when="midnight", interval=1, backupCount=7)
+handler.suffix = "%Y-%m-%d %H:%M:%S"
+logger.addHandler(handler)
 
 
 class LaunchedBot(object):
@@ -40,25 +43,21 @@ class LaunchedBot(object):
 def get_text_messages(message):
     try:
         launched_bot = LaunchedBot()
-        logging.debug(f"launched_bot = {launched_bot.get()}; "
-                      f"input text = {message.text}; "
-                      f"user: {message.from_user.id}; "
-                      f"{message.from_user.username}; "
-                      f"{message.from_user.last_name}; "
-                      f"{message.from_user.first_name}")
-        print(f"launched_bot = {launched_bot.get()}")
-        print(f"input text = {message.text}")
-        print(f"user: {message.from_user.id}; "
-              f"{message.from_user.username}; "
-              f"{message.from_user.last_name}; "
-              f"{message.from_user.first_name}")
-        print("----------------------------------------")
+        logger.info(f"launched_bot = {launched_bot.get()}; "
+                    f"input text = {message.text}; "
+                    f"user: {message.from_user.id}; "
+                    f"{message.from_user.username}; "
+                    f"{message.from_user.last_name}; "
+                    f"{message.from_user.first_name}")
         if message.text == "/word":
+            logger.info(f"user {message.from_user.first_name} start word bot")
             launched_bot.set(launched_bot="word")
         if message.text == "/candy":
+            logger.info(f"user {message.from_user.first_name} start candy bot")
             launched_bot.set(launched_bot="candy")
         if message.text == "/restart":
             launched_bot.set(launched_bot="")
+            logger.info(f"user {message.from_user.first_name} restarted")
         if launched_bot.get() == "word":
             words.word_bot(bot=bot, message=message)
         if launched_bot.get() == "candy":
@@ -69,8 +68,9 @@ def get_text_messages(message):
                              f"Удаление слов введи /word\n"
                              f"Игра конфеты введи /candy\n"
                              f"Перезапуск /restart\n")
-    except BaseException:
-        logging.exception(f"An exception was thrown! username = {message.from_user.username}")
+    except BaseException as ex:
+        logger.error(f"An exception was thrown! username = {message.from_user.username}")
+        logger.error(ex)
         bot.send_message(message.from_user.id, f"Произошла ошибка, мы работаем над устронением ошибки.")
         bot.send_message(message.from_user.id, f"Перезапуск.")
         bot.send_message(message.from_user.id,
@@ -81,7 +81,7 @@ def get_text_messages(message):
 
 
 if __name__ == "__main__":
-    logging.debug(f"----------------------------------------------------------------------------------")
-    logging.debug(f"----------------------------------START-------------------------------------------")
-    logging.debug(f"----------------------------------------------------------------------------------")
+    logger.info(f"----------------------------------------------------------------------------------")
+    logger.info(f"----------------------------------START-------------------------------------------")
+    logger.info(f"----------------------------------------------------------------------------------")
     bot.polling(none_stop=True, interval=0)
