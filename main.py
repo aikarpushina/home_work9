571107e92910741834caadf9b20a985df6d22211
lyonchikk1 <alena.karpushina2015@yandex.ru>
2022-12-01 23:45:11 +0300

    add rational calc

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import telebot
 import words
 import candies
 import logging
+from telebot import types
+from fractions import Fraction
 from logging.handlers import TimedRotatingFileHandler
 
 bot = telebot.TeleBot(security.get_token())
@@ -23,6 +25,8 @@ handler = TimedRotatingFileHandler(filename=file_name, when="midnight", interval
 handler.suffix = "%Y-%m-%d %H:%M:%S"
 logger.addHandler(handler)
 
+a, b = 0, 0
+act = ""
 
 class LaunchedBot(object):
     launched_bot = ""
@@ -55,6 +59,9 @@ def get_text_messages(message):
         if message.text == "/candy":
             logger.info(f"user {message.from_user.first_name} start candy bot")
             launched_bot.set(launched_bot="candy")
+        if message.text == "/calc":
+            logger.info(f"user {message.from_user.first_name} start calc bot")
+            launched_bot.set(launched_bot="calc")
         if message.text == "/restart":
             launched_bot.set(launched_bot="")
             logger.info(f"user {message.from_user.first_name} restarted")
@@ -62,11 +69,14 @@ def get_text_messages(message):
             words.word_bot(bot=bot, message=message)
         if launched_bot.get() == "candy":
             candies.candy_bot(bot=bot, message=message)
+        if launched_bot.get() == "calc":
+            calc_bot(message=message)
         if launched_bot.get() == "":
             bot.send_message(message.from_user.id,
                              f"Какого бота запустить?\n"
                              f"Удаление слов введи /word\n"
                              f"Игра конфеты введи /candy\n"
+                             f"Калькулятор /calc\n"
                              f"Перезапуск /restart\n")
     except BaseException as ex:
         logger.error(f"An exception was thrown! username = {message.from_user.username}")
@@ -78,6 +88,109 @@ def get_text_messages(message):
                          f"Удаление слов введи /word\n"
                          f"Игра конфеты введи /candy\n"
                          f"Перезапуск /restart\n")
+
+
+def calc_bot(message):
+    keyboard = types.InlineKeyboardMarkup()  # наша клавиатура
+    key_yes = types.InlineKeyboardButton(text='Рациональный', callback_data='rational')  # кнопка «Да»
+    keyboard.add(key_yes)  # добавляем кнопку в клавиатуру
+    key_no = types.InlineKeyboardButton(text='Комплексный ', callback_data='complex')
+    keyboard.add(key_no)
+    question = 'Какой калькулятор запустим?'
+    bot.send_message(message.chat.id, text=question, reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'rational')
+def call_calc_rational(call):
+    logger.info(f"user {call.message.from_user.id} {call.message.from_user.username}: start rational calc")
+    keyboard_rational = types.InlineKeyboardMarkup()  # наша клавиатура
+    key_plus = types.InlineKeyboardButton(text='a+b', callback_data='calc_plus')  # кнопка «Да»
+    key_minus = types.InlineKeyboardButton(text='a-b ', callback_data='calc_minus')
+    key_multiply = types.InlineKeyboardButton(text='a*b', callback_data='calc_multiply')  # кнопка «Да»
+    key_divide = types.InlineKeyboardButton(text='a/b ', callback_data='calc_divide')
+    keyboard_rational.add(key_plus, key_minus, key_multiply, key_divide)
+    question = 'Выберете операцию:'
+    bot.send_message(call.message.chat.id, text=question, reply_markup=keyboard_rational)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'complex')
+def call_calc_complex(call):
+    logger.info(f"user {call.message.from_user.id} {call.message.from_user.username}: start complex calc")
+    bot.send_message(call.message.chat.id, 'Упс... Пока не работает.')
+    bot.send_message(call.message.chat.id, f"Для выхода /restart")
+    calc_bot(call.message)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'calc_plus')
+def calc_plus_rational(call):
+    global act
+    act = "+"
+    logger.info(f"user {call.message.from_user.id} {call.message.from_user.username}: select {act} operation")
+    input_(call.message)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'calc_minus')
+def calc_plus_rational(call):
+    global act
+    act = "-"
+    logger.info(f"user {call.message.from_user.id} {call.message.from_user.username}: select {act} operation")
+    input_(call.message)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'calc_multiply')
+def calc_plus_rational(call):
+    global act
+    act = "*"
+    logger.info(f"user {call.message.from_user.id} {call.message.from_user.username}: select {act} operation")
+    input_(call.message)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'calc_divide')
+def calc_plus_rational(call):
+    global act
+    act = "/"
+    logger.info(f"user {call.message.from_user.id} {call.message.from_user.username}: select {act} operation")
+    input_(call.message)
+
+
+def input_(message):
+    bot.send_message(message.chat.id, 'Введите а (формат 2/1): ')
+    bot.register_next_step_handler(message, input_a)
+
+def input_a(message):
+    global a
+    a = Fraction(message.text)
+    logger.info(f"user {message.from_user.id} {message.from_user.username}: input a = {a}")
+    bot.send_message(message.chat.id, 'Введите b (формат 2/1): ')
+    bot.register_next_step_handler(message, input_b)
+
+
+def input_b(message):
+    global b
+    b = Fraction(message.text)
+    logger.info(f"user {message.from_user.id} {message.from_user.username} input a = {b}")
+    calculate(message)
+
+
+def calculate(message):
+    global a, b, act
+    if act == "+":
+        bot.send_message(message.chat.id, f'a + b = {a + b}')
+        logger.info(f"user {message.from_user.id} {message.from_user.username}: {a} {act} {b} = {a + b}")
+    elif act == "-":
+        bot.send_message(message.chat.id, f'a - b = {a - b}')
+        logger.info(f"user {message.from_user.id} {message.from_user.username}: {a} {act} {b} = {a - b}")
+    elif act == "*":
+        bot.send_message(message.chat.id, f'a * b = {a * b}')
+        logger.info(f"user {message.from_user.id} {message.from_user.username}: {a} {act} {b} = {a * b}")
+    elif act == "/":
+        bot.send_message(message.chat.id, f'a / b = {a / b}')
+        logger.info(f"user {message.from_user.id} {message.from_user.username}: {a} {act} {b} = {a / b}")
+    else:
+        bot.send_message(message.chat.id, f'Неверная операция.')
+        logger.info(f"user {message.from_user.id} {message.from_user.username}: выбрана неверная операция")
+    bot.send_message(message.from_user.id, f"Для выхода /restart")
+    calc_bot(message)
 
 
 if __name__ == "__main__":
